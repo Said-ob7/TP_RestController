025acff70ba6573f03e0256c5e3a72efdd0ef38e
Said-ob7 <saideloubid2@gmail.com>
2024-11-03 21:53:06 +0100

    first commit

---
+++ src/main/java/architecture/tp_restcontroller/TpRestControllerApplication.java
@@ -0,0 +1,13 @@
+package architecture.tp_restcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpRestControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpRestControllerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/architecture/tp_restcontroller/TpRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package architecture.tp_restcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpRestControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
